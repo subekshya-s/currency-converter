4b1801a33c4f0f80979eed4905d27ecb36d320b2
yojana29 <yojanapandey29@gmail.com>
2026-03-12 19:58:06 +0545

    Added currency rates based on the currency type

--- home.py
+++ home.py
@@ -4,43 +4,43 @@ Objective:
 This program asks the user to enter a currency type and an amount.
 It checks if the amount entered is a valid number.
 Then it displays the currency and amount entered by the user.
-# """
-# transactions = []
-# for i in range(4):  #task-3 as loop is said to be applied in the  same process as the task 1 and 2 
+ """
+transactions = []
+for i in range(4):  #task-3 as loop is said to be applied in the  same process as the task 1 and 2 
 
-#     # Ask the user to enter the currency type
-#     currency = input("Enter currency (USD or EUR or JPY or GBP): ")
+    # Ask the user to enter the currency type
+    currency = input("Enter currency (USD or EUR or JPY or GBP): ")
 
-#     # Ask the user to enter the amount
-#     amount = float(input("Enter amount: "))
-#     print("Currency entered:", currency)
-#     print("Amount entered:", amount)
-
-
-
-#     # ---------------- Task 2: Conversion ----------------
-#     if currency == "USD":
-#         converted_amount = amount * 132
-#     elif currency == "EUR":
-#         converted_amount = amount * 145
-#     elif currency == "JPY" :
-#         converted_amount = amount * 9.4
-#     elif currency == "GBP":
-#         converted_amount = amount * 198
-#     else:
-#         print("Unsupported currency. Conversion not possible.")
-#         exit()  # stop the program if invalid currency
-
-    # Print converted amount
-#     print("Converted amount in NPR:", converted_amount)
+    # Ask the user to enter the amount
+    amount = float(input("Enter amount: "))
+    print("Currency entered:", currency)
+    print("Amount entered:", amount)
 
 
-#     #----------------Task 4 : Transaction Report ----------
-#     print("\n======== Conversion History Report ========")
 
-# for i, transaction in enumerate(transactions, start=1):
-#     currency, amount, converted_amount = transaction
-#     print(f"Transaction {i}: {currency} {amount} -> NPR {converted_amount}")
+    # ---------------- Task 2: Conversion ----------------
+    if currency == "USD":
+        converted_amount = amount * 132
+    elif currency == "EUR":
+        converted_amount = amount * 145
+    elif currency == "JPY" :
+        converted_amount = amount * 9.4
+    elif currency == "GBP":
+        converted_amount = amount * 198
+    else:
+        print("Unsupported currency. Conversion not possible.")
+        exit()  # stop the program if invalid currency
+
+    #Print converted amount
+    print("Converted amount in NPR:", converted_amount)
+
+
+    #----------------Task 4 : Transaction Report ----------
+    print("\n======== Conversion History Report ========")
+
+for i, transaction in enumerate(transactions, start=1):
+    currency, amount, converted_amount = transaction
+    print(f"Transaction {i}: {currency} {amount} -> NPR {converted_amount}")
 
 
 """
@@ -73,4 +73,22 @@ exchange_rate = 133.5     # Example rate (1 unit foreign currency = 133.5 NPR)
 result = convert_currency(foreign_amount, exchange_rate)
 
 print("Converted amount in NPR:", result)
+
+#Tasks 2 Creating function get_currency_rate(currency) that returns the conversion rate based on the currency type
+
+def get_currency_rate(current):
+    if currency == "USD":
+        return 132
+    elif currency == "EUR":
+        return 145
+    elif currency == "INR":
+        return 1.6
+    elif currency == "JPY":
+        return 9.4
+    elif currency == "GBP":
+        return 198
+    else :
+        print("Unsupported currency ")
+        return None
+
     
