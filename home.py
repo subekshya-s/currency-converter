a758139508f07126668eaeba9075a18dface119d
subekshya-s <subekshyasubedi26@gmail.com>
2026-04-05 15:41:14 +0000

    feat: Linked the databse with the code

--- home.py
+++ home.py
@@ -1,43 +1,78 @@
-# # """TASK 1:
-# # Program: Currency Input Program
-# # Objective:
-# # This program asks the user to enter a currency type and an amount.
-# # It checks if the amount entered is a valid number.
-# # Then it displays the currency and amount entered by the user.
-# #  """
-# # transactions = []
-# for i in range(4):  #task-3 as loop is said to be applied in the  same process as the task 1 and 2 
+from db import create_database,get_connection
 
-# #     # Ask the user to enter the currency type
-#     currency = input("Enter currency (USD or EUR or JPY or GBP): ")
+#------------------------------------------------Table Creation--------------------------------------------------------------------------------------
 
-# #     # Ask the user to enter the amount
-#     amount = float(input("Enter amount: "))
-#     print("Currency entered:", currency)
-#     print("Amount entered:", amount)
+def create_table():
+    conn = get_connection()
+    with conn:
+        with conn.cursor() as cursor:
+            cursor.execute("""
+                CREATE TABLE IF NOT EXISTS currency_conversions (
+                    id SERIAL PRIMARY KEY,
+                    from_currency VARCHAR(10),
+                    to_currency VARCHAR(10),
+                    amount NUMERIC,
+                    converted_amount NUMERIC,
+                    conversion_rate NUMERIC
+                );
+            """)
+    conn.close()
+    print("Table created!")
 
-
-
-# #     # ---------------- Task 2: Conversion ----------------
-#     if currency == "USD":
-#         converted_amount = amount * 132
-#     elif currency == "EUR":
-#         converted_amount = amount * 145
-#     elif currency == "JPY" :
-#         converted_amount = amount * 9.4
-#     elif currency == "GBP":
-#         converted_amount = amount * 198
-#     else:
-#         print("Unsupported currency. Conversion not possible.")
-#         exit()  # stop the program if invalid currency
-
-# #     #Print converted amount
-#     print("Converted amount in NPR:", converted_amount)
+#------------------------------------------------------CRUD OPerations----------------------------------------------------------------------------------------------------
 
 
-# #     #----------------Task 4 : Transaction Report ----------
-# #     print("\n======== Conversion History Report ========")
 
+
+
+
+"""Task-1
+Program: Currency Input Program
+Objective:
+This program asks the user to enter a currency type and an amount.
+It checks if the amount entered is a valid number.
+Then it displays the currency and amount entered by the user.
+ """
+transactions = []
+for i in range(4):  #task-3 as loop is said to be applied in the  same process as the task 1 and 2 
+
+#     # Ask the user to enter the currency type
+    currency = input("Enter currency (USD or EUR or JPY or GBP): ")
+
+#     # Ask the user to enter the amount
+    amount = float(input("Enter amount: "))
+    print("Currency entered:", currency)
+    print("Amount entered:", amount)
+
+
+
+#     # ---------------- Task 2: Conversion ----------------
+    if currency == "USD":
+        converted_amount = amount * 132
+    elif currency == "EUR":
+        converted_amount = amount * 145
+    elif currency == "JPY" :
+        converted_amount = amount * 9.4
+    elif currency == "GBP":
+        converted_amount = amount * 198
+    else:
+        print("Unsupported currency. Conversion not possible.")
+        exit()  # stop the program if invalid currency
+
+#     #Print converted amount
+    print("Converted amount in NPR:", converted_amount)
+
+
+#     #----------------Task 4 : Transaction Report ----------
+#     print("\n======== Conversion History Report ========")
+
+
+"""
+Program: Currency Converter
+Description: This program defines a function convert_currency(amount, rate)
+that converts a foreign currency amount into Nepalese Rupees (NPR)
+using a given exchange rate.
+"""
 
 # """
 # Program: Currency Converter
@@ -46,78 +81,71 @@
 # using a given exchange rate.
 # """
 
-# # """
-# # Program: Currency Converter
-# # Description: This program defines a function convert_currency(amount, rate)
-# # that converts a foreign currency amount into Nepalese Rupees (NPR)
-# # using a given exchange rate.
-# # """
-
-# def convert_currency(amount, rate):
-#     """
-#     This function converts foreign currency to NPR.
+def convert_currency(amount, rate):
+    """
+    This function converts foreign currency to NPR.
     
-#     Parameters:
-#     amount : float -> foreign currency amount
-#     rate   : float -> exchange rate to NPR
+    Parameters:
+    amount : float -> foreign currency amount
+    rate   : float -> exchange rate to NPR
     
-#     Returns:
-#     float -> converted amount in NPR
-#     """
+    Returns:
+    float -> converted amount in NPR
+    """
     
-#     npr = amount * rate
-#     return npr
+    npr = amount * rate
+    return npr
 
 
 
 
-# # Example usage
-# # foreign_amount = 100      # Example foreign currency amount
-# # exchange_rate = 133.5     # Example rate (1 unit foreign currency = 133.5 NPR)
-# #result = convert_currency(foreign_amount, exchange_rate)
-# #print("Converted amount in NPR:", result)
+# Example usage
+# foreign_amount = 100      # Example foreign currency amount
+# exchange_rate = 133.5     # Example rate (1 unit foreign currency = 133.5 NPR)
+#result = convert_currency(foreign_amount, exchange_rate)
+#print("Converted amount in NPR:", result)
 
 
 
-# #Tasks 2 Creating function get_currency_rate(currency) that returns the conversion rate based on the currency type
+#Tasks 2 Creating function get_currency_rate(currency) that returns the conversion rate based on the currency type
 
-# def get_currency_rate(currency):
-#     if currency == "USD":
-#         return 132
-#     elif currency == "EUR":
-#         return 145
-#     elif currency == "INR":
-#         return 1.6
-#     elif currency == "JPY":
-#         return 9.4
-#     elif currency == "GBP":
-#         return 198
-#     else :
-#         print("Unsupported currency ")
-#         return None
+def get_currency_rate(currency):
+    if currency == "USD":
+        return 132
+    elif currency == "EUR":
+        return 145
+    elif currency == "INR":
+        return 1.6
+    elif currency == "JPY":
+        return 9.4
+    elif currency == "GBP":
+        return 198
+    else :
+        print("Unsupported currency ")
+        return None
     
 
-# currency_input = input("Enter the currency: ")
-# rate = get_currency_rate(currency_input)
+currency_input = input("Enter the currency: ")
+rate = get_currency_rate(currency_input)
 
-# if rate is not None:
-#     print(f"The conversion rate for {currency_input} is: {rate}")
-# else:
-#     print("Currency not supported")
-
-
-# #Task 3 Create a function save_conversion(currency, amount, result) that saves conversion history in currency_history.txt.
-
-# def save_conversion(currency, amount, result):
-
-#     file = open("currency_history.txt", "a")
-
-#     file.write(f"{currency} {amount} -> NPR {result}\n")
-
-#     file.close()
+if rate is not None:
+    print(f"The conversion rate for {currency_input} is: {rate}")
+else:
+    print("Currency not supported")
 
 
-    #Storing exchange rated using tuples
+#Task 3 Create a function save_conversion(currency, amount, result) that saves conversion history in currency_history.txt.
+
+def save_conversion(currency, amount, result):
+
+    file = open("currency_history.txt", "a")
+
+    file.write(f"{currency} {amount} -> NPR {result}\n")
+
+    file.close()
+
+
+    # Storing exchange rated using tuples
 
 exchanges_rates = [
     ("USD", "NPR", 132),
@@ -174,6 +202,11 @@ def print_conversion_report():
         converted_amount = request["Amount"] * get_rate_to_nrp(request["Source"])
         print(request["Name"], "converted", request["Amount"],request["Source"],"to",converted_amount,"NPR")
     
+# ----------------- Main Program -----------------
+if __name__ == "__main__":
+    # Ensure DB and table exist
+    create_database()
+    create_table()
 
 num_request = int(input("How many conversions do you want to enter?"))
 for i in range(num_request):
@@ -181,7 +214,7 @@ for i in range(num_request):
 
 print_conversion_report()
 
-create_table()
+
 
 
 
